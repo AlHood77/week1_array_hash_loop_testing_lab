ed1cb66f62f9f4898e03f54eff7cd90386cd3153
Alan <alanhood77@gmail.com>
2020-04-05 17:54:40 +0100

    initial commit

---
+++ start_point/my_functions.rb
@@ -0,0 +1,17 @@
+
+def add_array_lengths(array1, array2)
+    return (array1 + array2).length 
+end
+
+def sum_array (array)
+    array.sum
+end
+
+def is_item_in_array (array, item)
+    array.include? item   
+end
+
+def get_first_key(key)
+    key.keys[0]
+    
+end
